fac61879ebe2593dd267d61835a3b748e94b7b3f
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-01-18 12:53:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "ibm" {
 }
 
 # data "ibm_resource_group" "cos_group" {
-#   name = "Defaultabcdef123 "
+#   name = "Default"
 # }
 
 module "cos" {
@@ -12,7 +12,8 @@ module "cos" {
   //source = "../../modules/instance"
   # bind_resource_key = var.bind_resource_key
   service_name      = "test_cos_bucket"
-  resource_group_id = "2a374cc74db644b48b2bef47539cbfee"
+  #resource_group_id = "2a374cc74db644b48b2bef47539cbfee"
+  resource_group_id= "Default"
   plan              = "standard"
   region            = "global"
   create_timeout    = "15m"
