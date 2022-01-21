17d418e925085672d895525922ba198d764d41b5
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-01-21 10:04:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,9 @@ module "cos" {
   # bind_resource_key = var.bind_resource_key
   service_name      = "test_cos_bucket"
   #resource_group_id= "66e101aa10194a809c39227c95aa8e94" #p
-  resource_group_id= "959a39a9a19e46d9b7e772e239ddf25d"
-  plan              = "standard"
+  resource_group_id= "959a39a9a19e46d9b7e772e239ddf25d" #s
+  #plan              = "standard"
+  plan = "Lite"
   region            = "global"
   create_timeout    = "15m"
   update_timeout    = "15m"
