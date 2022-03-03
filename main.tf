fde6a8aa944bec11dbab2fe7e904b3f75d329901
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-03-03 21:53:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
-provider "ibm" {
-}
+# provider "ibm" {
+# }
 
 # data "ibm_resource_group" "cos_group" {
 #   name = "Default"
 # }
 
-module "cos" {
-  // Uncommnet the following line to point the source to registry level
-  source = "terraform-ibm-modules/cos/ibm//modules/instance"
+# module "cos" {
+#   // Uncommnet the following line to point the source to registry level
+#   source = "terraform-ibm-modules/cos/ibm//modules/instance"
 
-  //source = "../../modules/instance"
-  # bind_resource_key = var.bind_resource_key
-  service_name      = "test_cos_bucket"
-  resource_group_id= "66e101aa10194a809c39227c95aa8e94" #p
-#   resource_group_id= "959a39a9a19e46d9b7e772e239ddf25d" #s
-  plan              = "standard"
-#   plan = "lite"
-  region            = "global"
-  create_timeout    = "15m"
-  update_timeout    = "15m"
-  delete_timeout    = "15m"
+#   //source = "../../modules/instance"
+#   # bind_resource_key = var.bind_resource_key
+#   service_name      = "test_cos_bucket"
+#   resource_group_id= "66e101aa10194a809c39227c95aa8e94" #p
+# #   resource_group_id= "959a39a9a19e46d9b7e772e239ddf25d" #s
+#   plan              = "standard"
+# #   plan = "lite"
+#   region            = "global"
+#   create_timeout    = "15m"
+#   update_timeout    = "15m"
+#   delete_timeout    = "15m"
+# }
+
+provider "ibm" {
+    region ="us-south"
+}    
+
+data "ibm_resource_group" "resource_group" {
+    is_default = "true"
 }
 
-# provider "ibm" {
-#     region ="us-south"
-# }    
+resource "ibm_is_vpc" "vpc" {
+    name = "vpc1"
+}
 
-# data "ibm_resource_group" "resource_group" {
-#     is_default = "true"
-# }
-
-# resource "ibm_is_vpc" "vpc" {
-#     name = "vpc1"
-# }
-
-# resource "ibm_is_subnet" "subnet" {
-#     name                     = "subnet1"
-#     vpc                      = ibm_is_vpc.vpc.id
-#     zone                     = "us-south-1"
-#     total_ipv4_address_count = 256
-# }
+resource "ibm_is_subnet" "subnet" {
+    name                     = "subnet1"
+    vpc                      = ibm_is_vpc.vpc.id
+    zone                     = "us-south-1"
+    total_ipv4_address_count = 256
+}
 
