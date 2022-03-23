c7619cb0666448ecc7152f84e808451e24374199
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-03-23 15:41:39 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   required_providers {
-#     ibm = {
-#       source = "ibm-cloud/ibm"
-#       version = "1.25.0"
-#     }
-#   }
-#   required_version = ">= 0.13"
-# }
+terraform {
+  required_providers {
+    ibm = {
+      source = "ibm-cloud/ibm"
+      version = "1.25.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
